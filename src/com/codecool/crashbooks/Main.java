c11a9ebf95ce5bb0c9a607d7551a7ce76cc30e13
Adam Godor <adam.godor@gmail.com>
2018-02-07 13:31:53 +0100

    1.1/basic server initiated

--- src/com/codecool/crashbooks/Main.java
+++ src/com/codecool/crashbooks/Main.java
@@ -1,7 +1,22 @@
 package com.codecool.crashbooks;
 
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.enableDebugScreen;
+
+import spark.Request;
+import spark.Response;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+
 public class Main {
     public static void main(String[] args) {
 
+        exception(Exception.class, (e, req, res) -> e.printStackTrace());
+        staticFileLocation("/public");
+        port(8888);
+
+        get("/hello", (req, res) -> "Hello World!");
+
+        enableDebugScreen();
     }
 }
