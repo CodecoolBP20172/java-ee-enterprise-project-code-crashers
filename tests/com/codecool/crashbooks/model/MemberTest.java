295be111953ac43655d647a02cde6c765d701589
Dani Knoll <knoll.dani@gmail.com>
2018-02-21 13:40:15 +0100

    0/3/Test refactor.

--- tests/com/codecool/crashbooks/model/MemberTest.java
+++ tests/com/codecool/crashbooks/model/MemberTest.java
@@ -1,11 +1,8 @@
 package com.codecool.crashbooks.model;
 
-import com.codecool.crashbooks.controller.MemberController;
 import com.codecool.crashbooks.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,23 +12,19 @@ class MemberTest extends SetupAndTearDown {
     @Test
     @DisplayName("Get Member by ID, VALID")
     public void testGetMemberByIdValid(){
-        Member member = new Member("test1", "test1");
-        assertEquals(member.getName(), memberService.getMemberById(emf,1).getName());
+        assertEquals("test1", memberService.getMemberById(emf,1).getName());
     }
 
     @Test
     @DisplayName("Get Member by ID, INVALID")
     public void testGetMemberByIdInvalid(){
-        assertThrows(NoResultException.class, () -> {
-            memberService.getMemberById(emf, 3);
-        });
+        assertEquals(null, memberService.getMemberById(emf, 3));
     }
 
     @Test
     @DisplayName("Get Member by name, VALID")
     public void testGetMemberByNameValid(){
-        Member member = new Member("test1", "test1");
-        assertEquals(member.getName(), memberService.getMemberByName(emf, "test1").getName());
+        assertEquals("test1", memberService.getMemberByName(emf, "test1").getName());
     }
 
     @Test
@@ -43,9 +36,8 @@ class MemberTest extends SetupAndTearDown {
     @Test
     @DisplayName("Test SaveMember")
     public void testSaveMember(){
-        Member member = new Member("newMember","newPassword");
-        memberService.saveMember(emf, member.getName(), member.getPassword());
-        assertEquals(member.getName(), memberService.getMemberByName(emf,"newMember").getName());
+        memberService.saveMember(emf, "newMember", "newPassword");
+        assertEquals("newMember", memberService.getMemberByName(emf,"newMember").getName());
     }
 
 }
