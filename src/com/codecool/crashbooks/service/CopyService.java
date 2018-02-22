af311e88790b3f31415cd2e42432b9604b05762e
Ollári Márton <ollarimarton@gmail.com>
2018-02-22 10:56:34 +0100

    created CopyService from NamedQueries

---
+++ src/com/codecool/crashbooks/service/CopyService.java
@@ -0,0 +1,54 @@
+package com.codecool.crashbooks.service;
+
+import com.codecool.crashbooks.model.mediaproperty.Copy;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import java.util.List;
+
+public class CopyService {
+    private EntityManagerFactory emf;
+
+    public CopyService(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+
+    public List<Copy> getAllCopy() {
+        EntityManager em = emf.createEntityManager();
+        List<Copy> copyList = em.createNamedQuery("Copy.getAllCopy", Copy.class).getResultList();
+        em.close();
+        return copyList;
+    }
+
+    public Copy getCopyById(int id){
+        EntityManager em = emf.createEntityManager();
+        try {
+            Copy copy = em.createNamedQuery("Copy.getById", Copy.class)
+                    .setParameter("id", id).getSingleResult();
+            return copy;
+        } catch(NoResultException e){
+            return null;
+        } finally {
+            if(em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Copy> getCopyByStatus(String status){
+        EntityManager em = emf.createEntityManager();
+        try {
+            List<Copy> copyList = em.createNamedQuery("Copy.getByStatus", Copy.class)
+                    .setParameter("status", status).getResultList();
+            return copyList;
+        } catch(NoResultException e){
+            return null;
+        } finally {
+            if(em != null) {
+                em.close();
+            }
+        }
+    }
+}
