27f739144ba53eec4a7f674fd1c94bc813838c0c
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2018-03-06 14:51:02 +0100

    remove unwanted dependencies from pom, redo CopyServiceTest for Spring

--- tests/com/codecool/crashbooks/model/CopyServiceTest.java
+++ tests/com/codecool/crashbooks/model/CopyServiceTest.java
@@ -1,35 +1,47 @@
 package com.codecool.crashbooks.model;
 
 import com.codecool.crashbooks.service.CopyService;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-class CopyServiceTest extends SetupAndTearDown {
-    private CopyService copyService = new CopyService(emf);
+
+@RunWith(SpringRunner.class)
+//@DataJpaTest
+@SpringBootTest
+class CopyServiceTest {
+
+    @Autowired
+    private CopyService copyService;
 
     @Test
     @DisplayName("Get All copy")
-    public void testGetAllCopy(){
+    public void testGetAllCopy() {
         assertEquals(5, copyService.getAllCopy().size());
     }
 
     @Test
     @DisplayName("Get copy by Id, for NULL")
-    public void testGetCopyByIdForNull(){
+    public void testGetCopyByIdForNull() {
         assertNull(copyService.getCopyById(10));
     }
 
     @Test
     @DisplayName("Get copy by Status")
-    public void testGetCopyByStatus(){
+    public void testGetCopyByStatus() {
         assertEquals(2, copyService.getCopiesByStatus(CopyStatuses.AVAILABLE).size());
     }
 
     @Test
     @DisplayName("Get the first available copy")
-    public void testGetFirstAvailableCopy(){
+    public void testGetFirstAvailableCopy() {
         assertEquals("testMedia3", copyService.getFirstAvailableCopy(3).getMedia().getTitle());
     }
 
