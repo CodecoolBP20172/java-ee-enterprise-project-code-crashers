dfba4816a6659d19dd5a01b33d249d5a37eac871
Lajos Balázs Vlasics <vlasicslajos@gmail.com>
2018-03-07 14:16:39 +0100

    implemented startrent and endrent controllers

--- src/com/codecool/crashbooks/controller/RentController.java
+++ src/com/codecool/crashbooks/controller/RentController.java
@@ -6,9 +6,10 @@ import com.codecool.crashbooks.service.RentService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 
 import javax.servlet.http.HttpSession;
 
@@ -22,11 +23,33 @@ public class RentController {
     @Autowired
     CopyService copyService;
 
-    @RequestMapping(value = "/rent", method = RequestMethod.POST)
+    @RequestMapping(value = "/rent", method = RequestMethod.POST)  // TODO rename to pendingRent
     public String rentCopy(HttpSession session, HttpServletRequest req) {
         if (session.getAttribute("name") != null) {
             rentService.createRent(memberService.getMemberByName((String) session.getAttribute("name")), copyService.getFirstAvailableCopy(Integer.parseInt(req.getParameter("media_id"))));
         }
         return "redirect:/";
+    }
+
+    @RequestMapping(value = "/startrent/{rentId}", method = RequestMethod.GET)
+    public String startRent(Model model, HttpSession session, @PathVariable int rentId) {
+        if (String.valueOf(session.getAttribute("membership")).equals("ADMIN")) {
+            rentService.startRent(rentService.getRentById(rentId));
+            return "redirect:/admin";
+        } else {
+            model.addAttribute("error", "Forbidden! You don't have permission for this page.");
+            return "book/error";
+        }
+    }
+
+    @RequestMapping(value = "/endrent/{rentId}", method = RequestMethod.GET)
+    public String endRent(Model model, HttpSession session, @PathVariable int rentId) {
+        if (String.valueOf(session.getAttribute("membership")).equals("ADMIN")) {
+            rentService.endRent(rentService.getRentById(rentId));
+            return "redirect:/admin";
+        } else {
+            model.addAttribute("error", "Forbidden! You don't have permission for this page.");
+            return "book/error";
+        }
     }
 }
