8d4175d29be941d9b818d2581d22fba6db5eb6ff
Dani Knoll <knoll.dani@gmail.com>
2018-03-08 10:47:57 +0100

    Membership images added.

--- src/com/codecool/crashbooks/model/memberProperty/Membership.java
+++ src/com/codecool/crashbooks/model/memberProperty/Membership.java
@@ -3,10 +3,10 @@ package com.codecool.crashbooks.model.memberProperty;
 
 public enum Membership {
     ADMIN(0, "Not message", 0, "korona.png"),
-    FREE(1, "Basic membership", 0, "korona.png"),
-    BRONZE(5, "Bronze membership", 2000, "korona.png"),
-    SILVER(10, "Silver membership", 5000, "korona.png"),
-    GOLD(15, "Gold membership", 9000, "korona.png");
+    FREE(1, "Basic membership", 0, "free2.png"),
+    BRONZE(5, "Bronze membership", 2000, "bronze2.png"),
+    SILVER(10, "Silver membership", 5000, "silver2.png"),
+    GOLD(15, "Gold membership", 9000, "gold2.png");
 
     private final int limit;
     private final String message;
