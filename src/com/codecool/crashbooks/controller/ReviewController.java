06d7d537ffc3067d9bfb13beb149a1a402625897
Adam Godor <adam.godor@gmail.com>
2018-03-08 17:08:25 +0100

    review controller finished

--- src/com/codecool/crashbooks/controller/ReviewController.java
+++ src/com/codecool/crashbooks/controller/ReviewController.java
@@ -1,6 +1,9 @@
 package com.codecool.crashbooks.controller;
 
-import com.codecool.crashbooks.service.RatingService;
+import com.codecool.crashbooks.model.Media;
+import com.codecool.crashbooks.model.Member;
+import com.codecool.crashbooks.model.mediaProperty.Review;
+import com.codecool.crashbooks.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpRequest;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
     @Controller
@@ -17,14 +21,22 @@ import javax.servlet.http.HttpSession;
     public class ReviewController {
 
         @Autowired
-        RatingService reviewService;
+        ReviewService reviewService;
+        @Autowired
+        MemberService memberService;
+        @Autowired
+        MediaService mediaService;
+        @Autowired
+        RatingService ratingService;
+        @Autowired
+        RentService rentService;
 
         @RequestMapping(value="/review/{id}", method = RequestMethod.GET)
-        public String renderRatePage(HttpSession session, Model model, @PathVariable String id){
+        public String renderReviewPage(HttpSession session, Model model, @PathVariable String id){
             String name =(String) session.getAttribute("name");
-            if (name != null) {
+            if (name == null) {
                 model.addAttribute("user_information", "Please Log in!");
-                return "redirect:/login";
+                return "media/login";
             }
             model.addAttribute("memberName", name);
             model.addAttribute("mediumId", id);
@@ -32,8 +44,19 @@ import javax.servlet.http.HttpSession;
         }
 
         @RequestMapping(value="/review/{id}", method = RequestMethod.POST)
-        public String rate(HttpSession session, Model model, @PathVariable String id, HttpRequest req){
-
-            return "redirect:/medium/"+id;
+        public String review(HttpSession session, Model model, @PathVariable String id, HttpServletRequest req){
+            Member member = memberService.getMemberByName((String)session.getAttribute("name"));
+            Media media = mediaService.getMediasBy(Integer.parseInt(id));
+            Review review = new Review(req.getParameter("rating"), member, media);
+            reviewService.saveReview(review);
+            if (session.getAttribute("id")!= null){
+                model.addAttribute("userReview", reviewService.getReviewByMemberAndMedia((int)session.getAttribute("id"), Integer.parseInt(id)));
+                model.addAttribute("userRating", ratingService.getRatingByMemberAndMedia((int)session.getAttribute("id"), Integer.parseInt(id)));
+                model.addAttribute("memberName", member.getName());
+            }
+            model.addAttribute("medium", mediaService.getMediasBy(Integer.parseInt(id)));
+            model.addAttribute("nextAvailableRentDate", rentService.getNextAvailableRentDate(Integer.parseInt(id)));
+            model.addAttribute("averageRating", mediaService.getMediasBy(Integer.parseInt(id)).getAverageRating());
+            return "media/book_review";
         }
 }
